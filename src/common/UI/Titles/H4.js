8ef7131bce9e753ed791f219acbd145c67a9927f
Irina <irensaywhen@gmail.com>
2021-02-16 15:52:00 +0300

    Some clutch data

---
+++ src/common/UI/Titles/H4.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const H4 = ({ children }) => {
+  return <h4 className='title is-6'>{children}</h4>;
+};
+
+export default H4;
--- src/structure/transmission/Clutch.js
+++ src/structure/transmission/Clutch.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import H3 from '../../common/UI/Titles/H3';
+import H4 from '../../common/UI/Titles/H4';
 
 const Clutch = () => {
-  return <div>Clutch component</div>;
+  return (
+    <div>
+      <div className='block'>
+        <H3>Сцепление</H3>
+        <p>
+          Является первым устройством трансмиссии и предназначено для передачи
+          крутящего момента от двигателя к первичному валу коробки передач.
+        </p>
+        <p>
+          Чтобы переключение передач происходило плавно, нужно кратковременно
+          прекратить передачу крутящего момента с двигателя на коробку передач,
+          отделив двигатель от трансмиссии. Сцепление состоит из{' '}
+          <span className='has-text-weight-semibold'>привода</span> и{' '}
+          <span className='has-text-weight-semibold'>механизма сцепления</span>.
+        </p>
+      </div>
+      <div className='block'>
+        <H4>Привод выключения сцепления</H4>
+      </div>
+    </div>
+  );
 };
 
 export default Clutch;
