b8d3b870d09b46a2c2df77a504a6fe43acce255f
Irina <irensaywhen@gmail.com>
2021-02-21 12:10:01 +0300

    sliding animation for mobile menu

--- src/common/Layout/Navigation/Navigation.js
+++ src/common/Layout/Navigation/Navigation.js
@@ -1,21 +1,9 @@
-import React, { useState, useContext } from 'react';
-
-import { MobileMenuContext } from '../../../context/mobile-menu-context';
+import React from 'react';
 
 import NavbarBrand from './NavbarBrand';
 import Menu from './Menu';
 
 const Navigation = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  // const mobileMenuContext = useContext(MobileMenuContext);
-
-  // console.log(mobileMenuContext);
-
-  const toggleMenuHandler = () => {
-    setIsMenuOpen(prevIsMenuOpen => !prevIsMenuOpen);
-  };
-
   return (
     <nav className='navbar is-fixed-top'>
       <div className='container'>
