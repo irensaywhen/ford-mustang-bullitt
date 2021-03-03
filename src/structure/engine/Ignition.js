c2d2787646c94b2ff339928c77b82f0afcf3b572
Irina <irensaywhen@gmail.com>
2021-03-03 11:34:28 +0300

    ignition adjusted

--- src/structure/engine/Ignition.js
+++ src/structure/engine/Ignition.js
@@ -9,10 +9,6 @@ import H3 from '../../common/UI/Titles/H3';
 import H4 from '../../common/UI/Titles/H4';
 import TextSemibold from '../../common/UI/Text/TextSemibold';
 
-import PrimaryButton from '../../common/UI/Buttons/PrimaryButton';
-
-import { ModalContext } from '../../context/modal-context';
-
 const ignitionSystemPeculiarities = [
   'Точный контроль времени зажигания для каждого цилиндра по отдельности',
   'Уменьшение выбросов',
@@ -20,122 +16,7 @@ const ignitionSystemPeculiarities = [
   'Уменьшение потерей энергии',
 ];
 
-const warnings = [
-  {
-    description:
-      'Не водить машину по сухой траве (и похожим покрытиям). Система выбросов имеет высокие температуры. Такое вождение может стать причиной пожара.',
-  },
-  {
-    description: 'Не чинить систему выброса газов до ее полного остужения.',
-  },
-  {
-    description:
-      'Если запах отработанных газов присуствует в кабине, необходимо немедленно предоставить машину в сервис на осмотр.',
-  },
-];
-
-const catalyticConverterMaintainance = [
-  'Использовать только рекомендованное топливо',
-  'Не оставаться без топлива',
-  'Не выключать зажигание в процессе движения',
-  'Не разогревать двигатель более 10 секунд',
-  'Do not run the engine with a spark plug lead disconnected.',
-  'Do not push-start or tow-start your vehicle. Use booster cables. See Jump Starting the Vehicle (page 207).',
-];
-
-const definitions = [
-  {
-    header: 'Запас хода автомобиля',
-    body:
-      'Максимальное расстояние, которое может проехать автомобиль на одной заправке топлива',
-  },
-  {
-    header: 'Топливный бак',
-    body: 'Емкость для хранения топлива. Располагается сзади автомобиля.',
-  },
-  {
-    header: 'Топливопроводы',
-    body:
-      'Трубки, предназначенные для доставки бензина от топливного бака до карбюратора.',
-  },
-  {
-    header: 'Топливный фильтр',
-    body:
-      'Предназначен для тонкой очистки топлива, поступающего к топливному насосу.',
-  },
-  {
-    header: 'Топливный насос',
-    body:
-      'Предназначен для принудительной подачи топлива из бака в карбюратор. Необходим, так как топливный бак располагается ниже карбюратора.',
-  },
-  {
-    header: 'Воздушный фильтр',
-    body:
-      'Необходим для очистки воздуха, поступающего в цилиндры двигателя. Устанавливается на верхней части воздушной головины карбюратора.',
-  },
-  {
-    header: 'Карбюратор',
-    body:
-      'Предназначен для приготовления горючей смеси и подачи ее в цилиндры двигателя. В зависимости от режимов работы двигателя, карбюратор меняет количество горючей смеси, а также соотношение воздуха и бензина в ней.',
-  },
-  {
-    header: 'Нормальная горючая смесь',
-    body:
-      'Горючая смесь называется нормальной, если на одну часть бензина приходится 15 частей воздуха.',
-  },
-  {
-    header: 'Обедненная горючая смесь',
-    body: 'Смесь, где воздуха больше чем 15 частей на 1 часть бензина.',
-  },
-  {
-    header: 'Обогащенная горючая смесь',
-    body: 'Смесь, где воздуха меньше чем 15 частей на 1 часть бензина.',
-  },
-];
-
 const Ignition = () => {
-  const modalContext = useContext(ModalContext);
-
-  const openModal = () => {
-    // Set video content
-    modalContext.setModalContent(() => (
-      <iframe
-        title='differential'
-        className='has-ratio'
-        width='560'
-        height='315'
-        src='https://www.youtube.com/embed/3mz1BpIE-Ec'
-        frameborder='0'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowfullscreen
-      ></iframe>
-    ));
-
-    modalContext.showModal();
-  };
-
-  const warningsList = warnings.map((warning, index) => (
-    <BulletpointWithIcon
-      key={index}
-      icon={<i className='fas fa-exclamation-triangle'></i>}
-      iconWrapperClasses={['is-size-1', 'is-1', 'has-text-warning']}
-    >
-      <p>{warning.description}</p>
-    </BulletpointWithIcon>
-  ));
-
-  const catalyticMaintainanceList = catalyticConverterMaintainance.map(
-    (precaution, index) => (
-      <BulletpointWithIcon
-        key={index}
-        icon={<i className='fas fa-exclamation-triangle'></i>}
-        iconWrapperClasses={['is-size-1', 'is-1', 'has-text-warning']}
-      >
-        <p>{precaution}</p>
-      </BulletpointWithIcon>
-    )
-  );
-
   return (
     <React.Fragment>
       <div className='block'>
