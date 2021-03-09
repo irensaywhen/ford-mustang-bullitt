5a15270295669b0557da0c9788213093c5b74cbc
Irina <irensaywhen@gmail.com>
2021-03-09 18:04:47 +0300

    Fuses

--- src/structure/electronicSystems/ElectronicSystems.js
+++ src/structure/electronicSystems/ElectronicSystems.js
@@ -23,6 +23,9 @@ import H3 from '../../common/UI/Titles/H3';
 import TextSemibold from '../../common/UI/Text/TextSemibold';
 import CenteredParagraphLg from '../../common/UI/Paragraphs/CenteredParagraphLg';
 
+// Images
+import fugeTypes from '../../assets/img/electronics/fuse-types.jpg';
+
 import classes from '../../assets/scss/pages/heroes.module.scss';
 
 import { ModalContext } from '../../context/modal-context';
@@ -47,36 +50,22 @@ const consumersParts = [
     Component: StartingEngine,
   },
   {
-    tabName: 'Система освещения',
+    tabName: 'Система освещения и сигнализации',
     tabLink: 'lights',
     Component: Lights,
   },
 ];
 
-const fielEfficiencyAutonomy = {
+const fusesTable = {
+  thead: ['Обозначение', 'Тип'],
   tBody: [
-    ['Город', '16.4 л/100 км'],
-    ['Магистраль', '10.2 л/100 км'],
-    ['Комбинированный', '13.6 л/100 км'],
-    ['Запас хода', '588 км'],
-    [
-      <span>
-        Выброс CO<sub>2</sub>
-      </span>,
-      '319 г/км',
-    ],
-  ],
-};
-
-const basicCharacteristics = {
-  tBody: [
-    ['Двигатель', '5,0 l V8'],
-    ['Сила', '480 лошадиных сил, 7,000 об/мин (358 кВт)'],
-    ['Крутящий момент', '569 H/м, 4,600 об/мин'],
-    ['Зажигание', 'Атмосферное'],
-    ['Тип топлива', '16.4 л/100 км'],
-    ['Город', '16.4 л/100 км'],
-    ['Город', '16.4 л/100 км'],
+    ['A', 'Micro 2'],
+    ['B', 'Micro 3'],
+    ['C', 'Maxi'],
+    ['D', 'Mini'],
+    ['E', 'M Case'],
+    ['J', 'J Case'],
+    ['G', 'J Case Low Profile'],
   ],
 };
 
@@ -135,15 +124,42 @@ const ElectronicSystems = () => {
           </div>
         </div>
 
-        <H2 className='has-text-centered pb-4'>Общие характеристики</H2>
-        <div className='columns is-justify-content-center'>
-          <div className='column'>
-            <H3>Двигатель</H3>
-            <Table tbodyRows={fielEfficiencyAutonomy.tBody} />
-          </div>
-          <div className='column'>
-            <H3>Расход топлива</H3>
-            <Table tbodyRows={basicCharacteristics.tBody} />
+        <div className='block'>
+          <div className='columns is-justify-content-center'>
+            <div className='column is-12-mobile is-10-tablet is-8-desktop'>
+              <div className='block'>
+                <H2 className='has-text-centered'>Предохранители</H2>
+                <p>
+                  Часто неисправности, связанные с электрическими системами,
+                  могут быть связаны с пробоем предохранителей. <br></br>{' '}
+                  Предохранители нужны для защиты потребителей электрического
+                  тока. <br></br>
+                  Важно отметить, что перед заменой предохранителей необходимо
+                  выключить аккумулятор. <br></br>
+                  Заменять предохранители нужно на такие же (рассчитанные на
+                  такие же токи). Проверять перегоревшие предохранители нужно
+                  перед тем как приступать к попыткам починить другие элементы
+                  электрообрудования автомобиля.
+                </p>
+              </div>
+
+              <div className='block'>
+                <H3 className='has-text-centered'>Типы предохранителей</H3>
+                <div className='columns'>
+                  <div className='column is-6'>
+                    <Table
+                      theadRow={fusesTable.thead}
+                      tbodyRows={fusesTable.tBody}
+                    />
+                  </div>
+                  <div className='column is-6'>
+                    <figure className='image'>
+                      <img src={fugeTypes} alt=''></img>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </Section>
