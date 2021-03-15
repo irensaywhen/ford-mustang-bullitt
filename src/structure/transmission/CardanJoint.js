dde2f2f03c09d9efa7e00667e8d5bf82c90c0fe6
Irina <irensaywhen@gmail.com>
2021-03-15 19:06:25 +0300

    CardanJoint

--- src/structure/transmission/CardanJoint.js
+++ src/structure/transmission/CardanJoint.js
@@ -2,47 +2,24 @@ import React from 'react';
 
 import Accordion from '../../common/UI/Accordion/Accordion';
 import OrderedList from '../../common/UI/OrderedList';
+import Block from '../../common/UI/Wrappers/Block';
 
 import H3 from '../../common/UI/Titles/H3';
 import H4 from '../../common/UI/Titles/H4';
+import TextSemibold from '../../common/UI/Text/TextSemibold';
 
-const definitions = [
-  {
-    header: 'Шарниры с вилками и крестовинами',
-    body:
-      'Обеспечивают возможность передачи крутящего момента под изменяющимся углом. ',
-  },
-  {
-    header: 'Шлицевое соединение',
-    body:
-      'Компенсирует линейное перемещение карданной передачи относительно кузова автомобиля при изменении угла передачи крутящего момента.',
-  },
-  {
-    header: 'Эластичная муфта',
-    body:
-      'Служит для принятия ударной волны, проходящей по трансмиссии при грубой работе с педалью сцепления.',
-  },
-];
-
-const cardanJointParts = [
-  'Переднего и заднего валов',
-  'Промежуточной опоры с подшипником',
-  'Шарниров с вилками и крестовинами',
-  'Шлицевого соединения',
-  'Эластичной муфты',
-];
+// data
+import { definitions, cardanJointParts } from './data/cardanJoint';
 
 const CardanJoint = () => {
   return (
     <React.Fragment>
-      <div className='block'>
+      <Block>
         <H3>Карданная передача</H3>
         <p>
-          Есть только у{' '}
-          <span className='has-text-weight-semibold'>заднеприводных</span>{' '}
-          автомобилей и предназначена для передачи крутящего момента от
-          вторичного вала коробки передач к главной передаче под изменяющимся
-          углом.
+          Есть только у <TextSemibold>заднеприводных</TextSemibold> автомобилей
+          и предназначена для передачи крутящего момента от вторичного вала
+          коробки передач к главной передаче под изменяющимся углом.
           <br></br>
           <br></br>
           Состоит из:
@@ -50,8 +27,8 @@ const CardanJoint = () => {
         <p className='pt-2'>
           <OrderedList listItems={cardanJointParts} />
         </p>
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <H4>Назначение и принцип работы</H4>
         <p>
           Задний мост у автомобиля связан с кузовом не жестко. <br></br>В
@@ -74,10 +51,10 @@ const CardanJoint = () => {
           Линейное расстояние между главной передачей и коробкой передач также
           величина переменная. Для этого необходимо шлицевое соединение.
         </p>
-      </div>
-      <div className='block pt-4'>
+      </Block>
+      <Block className='pt-4'>
         <Accordion items={definitions}></Accordion>
-      </div>
+      </Block>
     </React.Fragment>
   );
 };
