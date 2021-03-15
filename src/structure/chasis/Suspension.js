adc17a179de1e2b9c00e20d5fa8463ac4f78130d
Irina <irensaywhen@gmail.com>
2021-03-15 20:15:20 +0300

    Suspension

--- src/structure/chasis/Suspension.js
+++ src/structure/chasis/Suspension.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 
 import Accordion from '../../common/UI/Accordion/Accordion';
-import BulletpointWithIcon from '../../common/UI/BulletpointWithIcon';
+import Block from '../../common/UI/Wrappers/Block';
+import VideoIframe from '../../common/UI/VideoIframe';
+import FaultsList from '../../common/UI/FaultsList';
 
 // Text
 import H3 from '../../common/UI/Titles/H3';
@@ -12,66 +14,8 @@ import PrimaryButton from '../../common/UI/Buttons/PrimaryButton';
 
 import { ModalContext } from '../../context/modal-context';
 
-const faults = [
-  {
-    title: 'Трещины в раме',
-    description:
-      'Могут возникать из-за ударов. Соединение сваркой недопустимо, потому что сварные швы имеют другую прочность и могут изменить геометрию автомобиля.',
-  },
-  {
-    title: 'Шум и стуки в подвеске',
-    description:
-      'Возникают из-за ослабления болтов креплений, износа шарниров, поломки пружин, неисправности амортизатора.',
-  },
-  {
-    title: 'Повышенный и неравномерный износ шин',
-    description:
-      'Возникает при нарушении углов установки колес, грубого стиля вождения, износа шарниров.',
-  },
-  {
-    title: 'Увод автомобиля в сторону',
-    description:
-      'Возникает при нарушении углов установки, деформации рычагов передней повески, неодинаковой жесткости пружин, неодинакового давления в шинах',
-  },
-  {
-    title: 'Повышенные вибрации',
-    description:
-      'Возникают из-за дисбаланса колес, износа деталей подвески, повреждения дисков колес и вздутий на боковине шин.',
-  },
-];
-
-const definitions = [
-  {
-    header: 'Зависимая подвеска',
-    body:
-      'Подвеска, в которой оба колеса одной оси связаны между собой жесткой балкой.',
-  },
-  {
-    header: 'Независимая подвеска',
-    body:
-      'Подвеска, в которой колеса одной оси автомобиля не связаны жестко друг с другом. При наезде на неровность, одно колесо может менять свое положение независимо от другого.',
-  },
-  {
-    header: 'Упругий элемент подвески',
-    body:
-      'Служит для смягчений ударов и колебаний, передаваемых от дороги к кузову.',
-  },
-  {
-    header: 'Гасящий элемент подвески - амортизатор',
-    body:
-      'Необходим для гашения колебаний кузова за счет сопротивления, возникающего при перетекании жидкости через калиброванные ответствия.',
-  },
-  {
-    header: 'Стабилизатор поперечной устойчивости',
-    body:
-      'Предназначен для повышения управляемости и уменьшения крена автомобиля на поворотах.',
-  },
-  {
-    header: 'Углы установки передних колес (развал-схождение)',
-    body:
-      'Обеспечивают устойчивое прямолинейное движение автомобиля, уменьшение усилия, прикладываемого к рулевому колесу на повороте, качение передних колес на поворотах без проскальзывания, самовозврат передних колес в прямолинейное положение по окночании поворота, смягчение ударов по подвеске, снятие излишних нагрузок с наиболее ответственных деталей и подшипников.',
-  },
-];
+// data
+import { faults, definitions } from './data/suspension';
 
 const Suspension = () => {
   const modalContext = useContext(ModalContext);
@@ -79,34 +23,18 @@ const Suspension = () => {
   const openModal = () => {
     // Set video content
     modalContext.setModalContent(() => (
-      <iframe
+      <VideoIframe
         title='How suspension work'
-        width='560'
-        height='315'
         src='https://www.youtube.com/embed/bSNQs3kj2d0'
-        frameborder='0'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowfullscreen
-      ></iframe>
+      ></VideoIframe>
     ));
 
     modalContext.showModal();
   };
 
-  const faultsList = faults.map((fault, index) => (
-    <BulletpointWithIcon
-      key={index}
-      icon={<i className='fas fa-tools'></i>}
-      iconWrapperClasses={['is-1']}
-    >
-      <h5 className='is-5 has-text-weight-bold'>{fault.title}</h5>
-      <p>{fault.description}</p>
-    </BulletpointWithIcon>
-  ));
-
   return (
     <React.Fragment>
-      <div className='block'>
+      <Block>
         <H3>Подвеска колес автомобиля</H3>
         <p className='pt-2'>
           Предназначена для смягчения и гашения колебаний, передаваемых от
@@ -116,20 +44,22 @@ const Suspension = () => {
           относительно колес, едущих по дороге.<br></br>В данной модели
           использована <TextSemibold>независимая</TextSemibold> подвеска.
         </p>
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <PrimaryButton onClick={openModal}>
           Узнать подробнее о принципе работы
         </PrimaryButton>
-      </div>
+      </Block>
 
-      <div className='block pt-5'>
-        <H4 className='pb-3'>Основные неисправности подвески</H4>
-        {faultsList}
-      </div>
-      <div className='block pt-4'>
+      <Block>
+        <H4 className='pb-3 is-size-4'>Основные неисправности подвески</H4>
+      </Block>
+      <Block>
+        <FaultsList faults={faults} />
+      </Block>
+      <Block className='pt-4'>
         <Accordion items={definitions}></Accordion>
-      </div>
+      </Block>
     </React.Fragment>
   );
 };
