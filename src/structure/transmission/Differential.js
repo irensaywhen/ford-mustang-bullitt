87982c758ae2227651c8a7be26dd2b164df9cfb3
Irina <irensaywhen@gmail.com>
2021-03-15 18:59:27 +0300

    differential

--- src/structure/transmission/Differential.js
+++ src/structure/transmission/Differential.js
@@ -1,29 +1,19 @@
 import React, { useContext } from 'react';
 
 import OrderedList from '../../common/UI/OrderedList';
-import BulletpointWithIcon from '../../common/UI/BulletpointWithIcon';
 import H3 from '../../common/UI/Titles/H3';
 import H4 from '../../common/UI/Titles/H4';
 import TextSemibold from '../../common/UI/Text/TextSemibold';
+import Block from '../../common/UI/Wrappers/Block';
+import VideoIframe from '../../common/UI/VideoIframe';
+import FaultsList from '../../common/UI/FaultsList';
 
 import PrimaryButton from '../../common/UI/Buttons/PrimaryButton';
 
 import { ModalContext } from '../../context/modal-context';
 
-const mainJointParts = ['Ведущей шестерни', 'Ведомой шестерни'];
-
-const faults = [
-  {
-    title: 'Шум при движении на большой скорости',
-    description:
-      'Возникает из-за износа шестерен, их неправильной регулеровке, или в случае отсутствия масла в картере главной передачи.',
-  },
-  {
-    title: 'Подтекание масла',
-    description:
-      'Может быть через сальники, неплотные соединения, в случае залипания сапуна.',
-  },
-];
+// data
+import { mainJointParts, faults } from './data/differential';
 
 const Differential = () => {
   const modalContext = useContext(ModalContext);
@@ -31,46 +21,29 @@ const Differential = () => {
   const openModal = () => {
     // Set video content
     modalContext.setModalContent(() => (
-      <iframe
+      <VideoIframe
         title='differential'
-        className='has-ratio'
-        width='560'
-        height='315'
         src='https://www.youtube.com/embed/3mz1BpIE-Ec'
-        frameborder='0'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowfullscreen
-      ></iframe>
+      ></VideoIframe>
     ));
 
     modalContext.showModal();
   };
 
-  const faultsList = faults.map((fault, index) => (
-    <BulletpointWithIcon
-      key={index}
-      icon={<i className='fas fa-tools'></i>}
-      iconWrapperClasses={['is-size-3', 'is-1']}
-    >
-      <h5 className='is-5 has-text-weight-bold'>{fault.title}</h5>
-      <p>{fault.description}</p>
-    </BulletpointWithIcon>
-  ));
-
   return (
     <React.Fragment>
-      <div className='block'>
+      <Block>
         <H3>Главная передача</H3>
         <p>
           Предназначена для увеличения крутящего момента и передачи его на
           полуоси колес под углом 90 градусов.
         </p>
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <H4 className='mb-2'>Состоит из</H4>
         <OrderedList listItems={mainJointParts} />
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <H3>Дифференциал</H3>
         <p>
           Предназначен для распределения крутящего момента между полуосями
@@ -79,22 +52,22 @@ const Differential = () => {
           проходить неодинаковый путь бес проскальзывания относительно покрытия
           дороги.
         </p>
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <H4>Главная передача и дифференциал</H4>
         <p>
           Работают вместе. Главная передача способствует передаче крутящего
           момента под углом, а дифференциал позволяет распределять крутящий
           момент в определенной пропорции, избавляя от заносов.
         </p>
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <PrimaryButton onClick={openModal}>
           Узнать подробнее о принципе работы
         </PrimaryButton>
-      </div>
+      </Block>
 
-      <div className='block pt-5'>
+      <Block className='pt-5'>
         <H4>Основные неисправности главной передачи и дифференциала</H4>
         <p className='pb-5'>
           Для увеличения срока службы главной передачи и дифференциала,
@@ -106,8 +79,8 @@ const Differential = () => {
           <TextSemibold>сапун</TextSemibold>, чтобы избежать повышения давления
           и частого вытекания масла.
         </p>
-        {faultsList}
-      </div>
+        <FaultsList faults={faults} />
+      </Block>
     </React.Fragment>
   );
 };
