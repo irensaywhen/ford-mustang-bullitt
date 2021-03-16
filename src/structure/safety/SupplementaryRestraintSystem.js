5a044355453595f6d80b9fae991e62e6c8586e8a
Irina <irensaywhen@gmail.com>
2021-03-16 20:32:10 +0300

    Airbags

--- src/structure/safety/SupplementaryRestraintSystem.js
+++ src/structure/safety/SupplementaryRestraintSystem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import OrderedList from '../../common/UI/OrderedList';
-
-import BulletpointWithIcon from '../../common/UI/BulletpointWithIcon';
+import Block from '../../common/UI/Wrappers/Block';
+import Figure from '../../common/UI/Figure/Figure';
+import WarningsList from '../../common/UI/WarningsList';
 
 // Text
 import H3 from '../../common/UI/Titles/H3';
@@ -14,57 +15,15 @@ import frontAirbag from '../../assets/img/safety/front-airbag.png';
 import sideAirbag from '../../assets/img/safety/side-airbag.png';
 import safetyCanopy from '../../assets/img/safety/safety-canopy.png';
 
-const warnings = [
-  {
-    description:
-      'Подушки безопасности срабатывают резко. Есть риск получения травмы от надувшейся подушки безопаности.',
-  },
-  {
-    description:
-      'Подушки безопасности должны применяться вместе с ремнями безопасности. Полагаясь исключительно на подушки безопасности, пассажиры и водитель увеличивают риск травм и смерти.',
-  },
-  {
-    description:
-      'Дети до 12 лет должны быть должным образом зафиксированы в специальном средстве.',
-  },
-  {
-    description:
-      'Область перед подушкой безопасности, такде как место хранения подушки безопасности должны быть свободны от посторонних предметов.',
-  },
-  {
-    description:
-      'Недопустимо пытаться чинить или вносить изменения в данную систему безопасности самостоятельно.',
-  },
-  {
-    description:
-      'После срабатывания данной системы безопасности, она не будет функционировать повторно.',
-  },
-  {
-    description:
-      'Рекомендуется иметь расстояние не менее 25 сантиметров между модулем подушки безопасности и грудью пассажира или водителя.',
-  },
-];
-
-const detectingFromntPassengerSteps = [
-  'Выключить двигатель и попросить пассажира сесть по центру, ровно.',
-  'Включить двигатель и попросить пассажира посидеть в фиксированном положении на протяжении двух минут',
-  'Если индикатор по прежнему не может зафиксировать наличие впереди сидящего пассажира, пассажиру лучше пересесть назад',
-];
+import {
+  warnings,
+  detectingFromntPassengerSteps,
+} from './data/supplementaryRestraintSystem';
 
 const SupplementaryRestraintSystem = () => {
-  const warningsList = warnings.map((warning, index) => (
-    <BulletpointWithIcon
-      key={index}
-      icon={<i className='fas fa-exclamation-triangle'></i>}
-      iconWrapperClasses={['is-size-1', 'is-1', 'has-text-warning']}
-    >
-      <p>{warning.description}</p>
-    </BulletpointWithIcon>
-  ));
-
   return (
     <React.Fragment>
-      <div className='block'>
+      <Block>
         <H3>Подушки безопасности</H3>
         <p>
           Являются частью пассивной безопасности и должны быть использованы
@@ -75,8 +34,8 @@ const SupplementaryRestraintSystem = () => {
           Подушки безопасности водителя и переднего пассажира срабатывают при
           серьезных лобовых и боковых столкновениях.
         </p>
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <H4>Система детектирования впереди сидящего пассажира</H4>
 
         <div className='columns is-mobile'>
@@ -89,26 +48,24 @@ const SupplementaryRestraintSystem = () => {
             </p>
           </div>
           <div className='column is-3'>
-            <figure className='image'>
-              <img src={frontAirbag} alt=''></img>
-            </figure>
+            <Figure img={{ src: frontAirbag }} />
           </div>
         </div>
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <p>
           Если пассажир сидит на переднем сидении, но система вылючена, это
           может означать, что пассажир сидит неправильно. <br></br>
         </p>
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <H5>
           В таком случае необходимо выполнить следующую последовательность
           действий:
         </H5>
         <OrderedList listItems={detectingFromntPassengerSteps} />
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <H4>Боковые подушки безопасности</H4>
         <div className='columns'>
           <div className='column is-7'>
@@ -118,13 +75,11 @@ const SupplementaryRestraintSystem = () => {
             </p>
           </div>
           <div className='column is-5'>
-            <figure className='image'>
-              <img src={sideAirbag} alt=''></img>
-            </figure>
+            <Figure img={{ src: sideAirbag }} />
           </div>
         </div>
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <H4>Коленные подушки безопасности</H4>
         <p>
           Коленная подушка безопасности водителя расположена под приборной
@@ -132,8 +87,8 @@ const SupplementaryRestraintSystem = () => {
           Коленная подушка безопасности впереди сидящего пассажира расположена
           под бардачком.
         </p>
-      </div>
-      <div className='block'>
+      </Block>
+      <Block>
         <H4>Навесные подушки безопасности</H4>
         <div className='columns'>
           <div className='column is-7'>
@@ -143,13 +98,16 @@ const SupplementaryRestraintSystem = () => {
             </p>
           </div>
           <div className='column is-5'>
-            <figure className='image'>
-              <img src={safetyCanopy} alt=''></img>
-            </figure>
+            <Figure img={{ src: safetyCanopy }} />
           </div>
         </div>
-      </div>
-      <div className='block'>{warningsList}</div>
+      </Block>
+      <Block>
+        <H4>Предостережения, касающиеся подушек безопасности:</H4>
+      </Block>
+      <Block>
+        <WarningsList warnings={warnings} />
+      </Block>
     </React.Fragment>
   );
 };
